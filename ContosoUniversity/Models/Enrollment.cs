63afed709e0eac939e38f17c976e5dc4ce08ace1
beca2 <beca2@I4Y-DV4>
2016-12-28 09:10:05 +0100

    Agregar archivos de proyecto.

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+            //La propiedad grado es un enum. El signo de pregunta que viene abajo en Grade indica
+            //que la propiedad de grado es nullable. Un grado que es null es diferente de cero.
+            //Null significa que no se sabe o no ha sido asignado todavia.
+    }
+         public class Enrollment
+         {
+             public int EnrollmentID { get; set; }//La propiedad de Enrollmentid es una llave primaria.
+             public int CourseID { get; set; }//La propiedad CourseID es una llave de fuera .
+             public int StudentID { get; set; }//La propiedad StudenID es una llave de fuera que corresponde con la
+        //propiedad de navegación de Student.
+             public Grade? Grade { get; set; }
+
+             public virtual Course Course { get; set; }
+             public virtual Student Student { get; set; }
+        //La navigation properties esta normalmente definida como virtual por eso  tiene la funcionalidad Entity Framework como un lazy loading
+         }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }//La propiedad ID se convertira en la llave primaria de la columna de nuestra base de datos
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        //Enrollments es una navigation property que junta otras entidades que tienen relación.
+        //Por lo tanto la propiedad enrollments de una entidad estudiante tendrá todas las enrollment entidades que estan relacionadas con la 
+        //entidad estudiante.
+     }
+}
